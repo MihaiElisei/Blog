bc0c96ec786a9690967a2ec9b73b0a27f4af7a47
Mihai Elisei <mihai.m.elisei@gmail.com>
2024-08-26 20:10:05 +0300

    Initial Commit

---
+++ src/test/java/com/itschool/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itschool.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
